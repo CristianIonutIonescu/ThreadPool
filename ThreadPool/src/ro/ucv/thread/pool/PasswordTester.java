92bc0f78a4463b458efb3dca8e34d89a793bd83b
CristianIonutIonescu <cristian.ionut.ionescu@gmail.com>
2015-03-24 21:53:37 +0200

    Minor changes to thread pool for sha256

--- ThreadPool/src/ro/ucv/thread/pool/PasswordTester.java
+++ ThreadPool/src/ro/ucv/thread/pool/PasswordTester.java
@@ -6,7 +6,10 @@ import java.util.concurrent.Callable;
 
 public class PasswordTester implements Callable<String> {
 
-	String myName="";
+	String m_name="";
+	String m_hash="";
+	int m_intervalStart;
+	int m_intervalEnd;
 	//TODO: check off all the todos
 	
 	/**
@@ -19,10 +22,12 @@ public class PasswordTester implements Callable<String> {
 	 * @param hash String
 	 * @param name name of the PasswordTester instance
 	 */
-	public PasswordTester(String name){
-		myName=name;//save name locally in this class
+	public PasswordTester(String name,String hash,int intervalStart,int intervalEnd){
+		m_name=name;//save name locally in this class
 		
-		// at this point , name dies!!
+		m_hash = hash;
+		m_intervalStart = intervalStart;
+		m_intervalEnd = intervalEnd;
 	}
 	
 	
@@ -30,16 +35,12 @@ public class PasswordTester implements Callable<String> {
 	@Override
 	public String call() throws Exception {
 		
-		System.out.println(myName+Thread.currentThread().getId()+": i'm starting");
+		System.out.println(m_name+Thread.currentThread().getId()+": i'm starting");
 		
 		//TODO: all passwords from intervalStart to intervalEnd must be hashed using sha256 
 		//TODO: and checked if their sha256 hash is equal to  the locally saved hash from the constructor
 		
-		for(int i=0;i<10000000;i++){
-			//System.out.println(myName+Thread.currentThread().getId()+": i="+i);
-		}
-		
-		return myName+": i finished !"; 
+		return m_name; 
 		//return null;
 	}
 	
--- ThreadPool/src/ro/ucv/thread/pool/RunMe.java
+++ ThreadPool/src/ro/ucv/thread/pool/RunMe.java
@@ -20,7 +20,7 @@ public class RunMe {
 			
 			//TODO: divide your problem into manageable intervals here
 			
-		   PasswordTester task = new PasswordTester("Johnny"+i+"_");
+		   PasswordTester task = new PasswordTester("Thread" + i,"ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f", Integer.MIN_VALUE,Integer.MAX_VALUE);
 		   collection.add(task);
 		  }
 		
